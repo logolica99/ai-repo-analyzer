201e5df195ed9c6042e9981b3f90be20ad432962
logolica99 <jamijubaer@gmail.com>
2025-09-16 15:55:11 -0700

    udpats

--- nextjs-ui/run.sh
+++ nextjs-ui/run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+
+
+# Install dependencies
 npm i
 
 # Run build and check for success
-if npm run build; then
+if npm run build
     echo "✅ Build succeeded. Restarting PM2 services..."
+    
+    # Stop and restart PM2 processes only if build succeeds
     pm2 delete all
     pm2 start "npm start -- -p 4173" --name "rerite-frontend-0"
     pm2 start "npm start -- -p 4174" --name "rerite-frontend-1"
+  
+    
 else
     echo "❌ Build failed! Keeping the previous version running."
     exit 1  # Exit without stopping the running version
-fi
+end
+
+
