e8d400541cda63de1315c46fa2c8599f3a097d39
logolica99 <jamijubaer@gmail.com>
2025-09-16 15:52:26 -0700

    fixed running command

--- nextjs-ui/run.sh
+++ nextjs-ui/run.sh
@@ -2,14 +2,12 @@
 npm i
 
 # Run build and check for success
-if npm run build
+if npm run build; then
     echo "✅ Build succeeded. Restarting PM2 services..."
     pm2 delete all
     pm2 start "npm start -- -p 4173" --name "rerite-frontend-0"
     pm2 start "npm start -- -p 4174" --name "rerite-frontend-1"
-
-
 else
     echo "❌ Build failed! Keeping the previous version running."
     exit 1  # Exit without stopping the running version
-end
+fi
